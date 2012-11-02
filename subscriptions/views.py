5583ddd360947fe0fa9be21bdc90c6a5e03b52b0
Dee Jay <bedokrez@gmail.com>
2012-11-01 20:06:21 -0400

    Update

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -9,9 +9,9 @@ import datetime
 
 def index(request):
     now = datetime.datetime.now()
-    t = get_template('test.html')
+    t = get_template('index.html')
     message = "Hello, world. You're at the poll index."
     number = 25
 #    html = t.render(Context({'current_date': message}))
 #    return HttpResponse(html)
-    return render_to_response('test.html', {'current_date': message})
+    return render_to_response('index.html', {'current_date': message})
